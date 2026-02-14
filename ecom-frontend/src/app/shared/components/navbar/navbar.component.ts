fde0720ba5fce6fa5aad050be0ebc974dd2b6298
AhmadDhariwal <ahmaddhariwal023@gmail.com>
2026-02-14 22:17:01 +0500

    auto-commit

--- ecom-frontend/src/app/shared/components/navbar/navbar.component.ts
+++ ecom-frontend/src/app/shared/components/navbar/navbar.component.ts
@@ -4,17 +4,28 @@ import { RouterModule, Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { AuthService } from '../../../core/services/auth.service';
 import { CartService } from '../../../core/services/cart.service';
+import { SidebarComponent } from '../sidebar/sidebar.component';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule, RouterModule, FormsModule],
+  imports: [CommonModule, RouterModule, FormsModule, SidebarComponent],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
   showSearch = false;
   searchQuery = '';
+  showMenu = false;
+
+  toggleMenu() {
+    this.showMenu = !this.showMenu;
+    if (this.showMenu) {
+      document.body.style.overflow = 'hidden'; // Prevent scrolling when menu is open
+    } else {
+      document.body.style.overflow = '';
+    }
+  }
 
   constructor(
     public authService: AuthService,
---
+++ ecom-frontend/src/app/shared/components/sidebar/sidebar.component.ts
@@ -0,0 +1,22 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { AuthService } from '../../../core/services/auth.service';
+
+@Component({
+  selector: 'app-sidebar',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './sidebar.component.html',
+  styleUrl: './sidebar.component.scss'
+})
+export class SidebarComponent {
+  @Input() isOpen = false;
+  @Output() close = new EventEmitter<void>();
+
+  constructor(public authService: AuthService) {}
+
+  closeSidebar() {
+    this.close.emit();
+  }
+}
